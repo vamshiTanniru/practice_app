a9a7e08b3b06f048f8afb63994eacfecb44250c0
vamshiTanniru <119478238+vamshiTanniru@users.noreply.github.com>
2023-03-01 10:29:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 import streamlit as st
 
-st.title("Hi my self Vamshi prakash")
-st.snow()
+st.title('Hello Guysss :wave: I am :blue[Vamshi Prakash Tanniru]')
 
-btn_click = st.button("Click Me!")
+st.subheader("I have complited my B.tech in (ECE) Engineering. I am interested in problem-solving with an eye toward the fascinating world of Artificial Intelligence and I have a good understanding of Python . Being an AI Enthusiast :sunglasses: I am here to learn and explore more in this domain!")
 
-if btn_click == True:
-    st.subheader("You clicked me :cry:")
+st.header("You can connect with me through :handshake: :")
+
+# x = st.slider('x')  # 👈 this is a widget
+# st.write(x, 'squared is', x * x)
+
+st.write(":safety_pin: [Github](https://github.com/vamshiTanniru)")
+
+st.write(":pushpin: [Linkedin](https://www.linkedin.com/in/tanniru-vamshi-prakash-82b0a0257/)")
+
+st.write(":e-mail: [Email](vamshiprakash001@gmail.com)")
+
+
+st.text("")
+st.text("")
+if st.button('Have a great day!'):
     st.balloons()
